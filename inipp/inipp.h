261ba2633a5262570207f68e60c0e68809f94c55
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-04-16 12:54:20 +0100

    Update copyright year.

--- inipp/inipp.h
+++ inipp/inipp.h
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2017-2018 Matthias C. M. Troffaes
+Copyright (c) 2017-2020 Matthias C. M. Troffaes
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
